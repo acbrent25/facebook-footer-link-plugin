7d73ae5b3c17a0164149467c6db7ad8db6cb87a0
acbrent25 <adamchampagne@gmail.com>
2019-04-05 17:11:14 -0400

    plugin header

---
+++ facebook-footer-link.php
@@ -0,0 +1,14 @@
+<?php
+/*
+Plugin Name: Facebook Footer Link 
+Description: Ads a Facebook profile link to the end of posts
+Version: 1.0
+Author: Adam Champagne
+Auther URI: https://adamchampagne.com/
+Text Domain: facebook-footer-link-plugin
+*/
+
+ // Exit if Accessed Directly
+ if(!defined('ABSPATH')){
+     exit;
+ }
