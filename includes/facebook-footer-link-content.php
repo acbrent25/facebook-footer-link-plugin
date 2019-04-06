53927012e4d01a73648fcf0ddd37aa5485bee8a8
acbrent25 <adamchampagne@gmail.com>
2019-04-06 12:12:45 -0400

    optimized code

--- includes/facebook-footer-link-content.php
+++ includes/facebook-footer-link-content.php
@@ -3,20 +3,29 @@
 function ffl_add_footer($content){
 
     global $ffl_options;
+    ob_start(); ?>
 
-    $footer_output = '<hr>';
-    $footer_output .= '<div class="footer_content">';
-    $footer_output .= '<i class="fab fa-facebook-square"></i> Find me on <a style="color:'. $ffl_options['link_color'] .'" href="' . $ffl_options['facebook_url'] . '" target="_blank">Facebook</a><br>';
-    $footer_output .= '<i class="fab fa-instagram"></i> Find me on <a style="color:'. $ffl_options['link_color'] .'" href="' . $ffl_options['instagram_url'] . '" target="_blank">Instagram</a>';
-    $footer_output .= '</div>';
+    <hr>
+    <div class="footer_content">
+        <ul class="footer-social-links">
+            <li class="footer-social-link">
+                <a style="color:<?php echo $ffl_options['link_color'] ?>" href="<?php echo $ffl_options['facebook_url']; ?>" target="_blank"><i class="fab fa-facebook-square"></i></a>
+            </li>  
+            <li>
+                <a style="color:<?php echo $ffl_options['link_color'] ?>" href="<?php echo $ffl_options['instagram_url']; ?>" target="_blank"><i class="fab fab fa-instagram"></i></a>
+            </li>     
+        </ul>
+    </div>
+
+    <?php 
 
     if($ffl_options['show_in_feed']){
         if(is_single() || is_home() && $ffl_options['enable']){
-            return $content . $footer_output;
+            return $content . ob_get_clean();
         }
     } else {
         if(is_single() && $ffl_options['enable']){
-            return $content . $footer_output;
+            return $content . ob_get_clean();
         }
     }
 
